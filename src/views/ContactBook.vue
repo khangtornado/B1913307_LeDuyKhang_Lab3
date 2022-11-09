6114902e6d6b8790195c217b6388d8740c3ea4d2
khangtornado <leduykhang9a7@gmail.com>
2022-11-09 16:46:41 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -39,6 +39,16 @@
                 </h4>
             </div>
             <ContactCard :contact="activeContact" />
+            <router-link
+                :to="{
+                    name: 'contact.edit',
+                    params: { id: activeContact._id },
+                }"
+            >
+                <span class="mt-2 badge badge-warning">
+                    <i class="fas fa-edit"></i> Hiệu chỉnh</span
+                >
+            </router-link>
         </div>
     </div>
 </template>
